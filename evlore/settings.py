ca2d51e40a67eb01a40b920cef373c846841fa3b
Basel Hussein <cv98@windowslive.com>
2024-07-08 16:32:27 +0000

    add sitting to contexts

--- evlore/settings.py
+++ evlore/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -166,3 +167,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 0
+STANDARD_DELIVERY_PERCENTAGE = 0
