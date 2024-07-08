1c09c76918f49414ee5ea296babb763c9ac5e76a
Basel Hussein <cv98@windowslive.com>
2024-07-08 10:28:20 +0000

    add current categories

--- products/views.py
+++ products/views.py
@@ -31,6 +31,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
