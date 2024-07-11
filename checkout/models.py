b908010dc1303987651ca967c5265261e5576c9b
Basel Hussein <cv98@windowslive.com>
2024-07-11 10:49:47 +0000

    creat generate order

--- checkout/models.py
+++ checkout/models.py
@@ -30,6 +30,14 @@ class Order(models.Model):
     stripe_pid = models.CharField(max_length=254, null=False, blank=False,
                                     default='')
 
+
+def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False,
                                 on_delete=models.CASCADE,
