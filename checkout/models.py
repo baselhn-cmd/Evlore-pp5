de1b941ec63d62bdfadd5cc181ff767ce9ac6fc5
Basel Hussein <cv98@windowslive.com>
2024-07-11 10:46:30 +0000

    creat class order line item and import uuid

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,9 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -25,3 +30,15 @@ class Order(models.Model):
     stripe_pid = models.CharField(max_length=254, null=False, blank=False,
                                     default='')
 
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False,
+                                on_delete=models.CASCADE,
+                                related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True,
+                                    blank=True)  
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                            null=False, blank=False,
+                                            editable=False)
