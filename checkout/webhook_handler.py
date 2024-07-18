507785deff0508e83685aff607bc953722f8a537
Basel Hussein <cv98@windowslive.com>
2024-07-18 12:44:33 +0000

    added new public ancecret key for test

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,9 +2,7 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """
-    Handle Stripe webhooks
-    """
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -16,19 +14,19 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
         return HttpResponse(
-            content=f'webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
-    
-    def handle_payment_intent_failed(self, event):
+
+    def handle_payment_intent_payment_failed(self, event):
         """
-        Handle the payment_intent.pyment_faild webhook from Stripe
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
-            content=f'webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]}',
             status=200)
