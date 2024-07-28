8088cd646e7f895c38d292b213e1c826da697f64
Basel Hussein <cv98@windowslive.com>
2024-07-28 09:26:45 +0000

    added new aws storge

--- evlore/settings.py
+++ evlore/settings.py
@@ -203,7 +203,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'evlorepp5'
+    AWS_STORAGE_BUCKET_NAME = 'evlore'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
